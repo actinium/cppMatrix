087d365eb12943b25fab0f798832b817620be1ee
actinium <andreas.cederholm@gmail.com>
2016-10-01 02:48:40 +0200

    Add rows and cols assignment.

--- include/matrix_assignment_impl.h
+++ include/matrix_assignment_impl.h
@@ -6,6 +6,8 @@ template<class T>
 matrix<T>& matrix<T>::operator=(
     std::initializer_list<std::initializer_list<T>> init_list){
   data_ = make_vector(init_list);
+  rows_ = init_list.size();
+  cols_ = (init_list.size() > 0) ? init_list.begin()->size() : 0;
   return this;
 }
 
