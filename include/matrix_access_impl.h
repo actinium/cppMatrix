0d285e1e9838ce53a5cc4e955c70024fe5000e12
actinium <andreas.cederholm@gmail.com>
2016-10-01 02:07:27 +0200

    Implement at().

--- include/matrix_access_impl.h
+++ include/matrix_access_impl.h
@@ -3,22 +3,22 @@
 //-----------------------------------------------------------------------------
 
 template<class T>
-T& matrix<T>::at( std::size_t, std::size_t ){
+T& matrix<T>::at( std::size_t row, std::size_t col ){
+  return data_.at(row*cols_+col);
+}
+
+template<class T>
+const T& matrix<T>::at( std::size_t row, std::size_t col ) const{
+  return data_.at(row*cols_+col);
+}
+
+template<class T>
+typename matrix<T>::matrix_row matrix<T>::operator[]( std::size_t row ){
   return 0;
 }
 
 template<class T>
-const T& matrix<T>::at( std::size_t, std::size_t ) const{
-  return 0;
-}
-
-template<class T>
-typename matrix<T>::matrix_row matrix<T>::operator[]( std::size_t ){
-  return 0;
-}
-
-template<class T>
-const typename matrix<T>::matrix_row matrix<T>::operator[]( std::size_t ) const{
+const typename matrix<T>::matrix_row matrix<T>::operator[]( std::size_t row ) const{
   return 0;
 }
 
