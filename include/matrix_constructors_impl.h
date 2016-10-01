20c2122027dab79bda516c91fe924f23bc938794
actinium <andreas.cederholm@gmail.com>
2016-10-01 02:44:04 +0200

    Add check for empty matrix in constructor.

--- include/matrix_constructors_impl.h
+++ include/matrix_constructors_impl.h
@@ -3,19 +3,20 @@
 //-----------------------------------------------------------------------------
 
 template<class T>
-matrix<T>::matrix():data_(),rows_(0),cols_(0){}
+matrix<T>::matrix():
+    data_(),
+    rows_(0),
+    cols_(0) {}
 
 template<class T>
 matrix<T>::matrix( std::size_t rows, std::size_t cols ):
-    data_(rows*cols),rows_(rows),cols_(cols){}
+    data_(rows*cols),
+    rows_(rows),
+    cols_(cols) {}
 
 template<class T>
 matrix<T>::matrix( std::initializer_list<std::initializer_list<T>> init_list):
     data_( make_vector(init_list) ),
-    rows_(init_list.size()),cols_( init_list.begin()->size() ){}
-    // !!!
-    //
-    // Is "init_list.begin()->size()" ok if init_list = {} ?
-    //
-    // !!!
+    rows_(init_list.size()),
+    cols_( (init_list.size() > 0) ? init_list.begin()->size() : 0 ) {}
 
