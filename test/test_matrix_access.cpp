8a931e6fd26652694151d5d69784f90192bba197
actinium <andreas.cederholm@gmail.com>
2016-10-02 23:21:08 +0200

    Add testcases for access.

--- test/test_matrix_access.cpp
+++ test/test_matrix_access.cpp
@@ -15,3 +15,18 @@ TEST_CASE( "Test matrix access out of range", "[matrix][access]" ) {
     REQUIRE_THROWS_AS( m.at(2,2), std::out_of_range );
   }
 }
+
+TEST_CASE( "Test changing elements", "[matrix][access]" ) {
+  SECTION("Try changing a value in an empty matrix"){
+    matrix<int> m = {};
+    REQUIRE_THROWS_AS( m.at(0,0) = 1, std::out_of_range );
+  }
+  SECTION("Try changing a value in an out of range column"){
+    matrix<int> m(2,3);
+    REQUIRE_THROWS_AS( m.at(1,3) = 1, std::out_of_range );
+  }
+  SECTION("Try changing a value in an out of range row"){
+    matrix<int> m(2,3);
+    REQUIRE_THROWS_AS( m.at(2,2) = 1, std::out_of_range );
+  }
+}
