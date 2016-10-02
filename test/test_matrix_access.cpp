c04fb13db78e25f4a435175f9756e9a185867077
actinium <andreas.cederholm@gmail.com>
2016-10-02 23:43:10 +0200

    Add access test cases.

--- test/test_matrix_access.cpp
+++ test/test_matrix_access.cpp
@@ -17,6 +17,37 @@ TEST_CASE( "Test matrix access out of range", "[matrix][access]" ) {
 }
 
 TEST_CASE( "Test changing elements", "[matrix][access]" ) {
+  SECTION("Test changing an element in a 1x1 matrix using at()"){
+    matrix<int> m(1,1);
+    REQUIRE( m.at(0,0) == 0 );
+    m.at(0,0) = 1;
+    REQUIRE( m.at(0,0) == 1 );
+  }
+  SECTION("Test changing an element in a 1x1 matrix using square brackets"){
+    matrix<int> m(1,1);
+    REQUIRE( m.at(0,0) == 0 );
+    m[0][0] = 1;
+    REQUIRE( m.at(0,0) == 1 );
+  }
+  SECTION("Test changing an element using another element"){
+    matrix<int> m = {{1,2},{3,4}};
+    m[0][1] = m[1][0];
+    REQUIRE( m.at(0,0) == 1 );
+    REQUIRE( m.at(0,1) == 3 );
+    REQUIRE( m.at(1,0) == 3 );
+    REQUIRE( m.at(1,1) == 4 );
+  }
+  SECTION("Test changing an element using the same element"){
+    matrix<int> m = {{1,2},{3,4}};
+    m[0][1] = m[0][1] + 4;
+    REQUIRE( m.at(0,0) == 1 );
+    REQUIRE( m.at(0,1) == 6 );
+    REQUIRE( m.at(1,0) == 3 );
+    REQUIRE( m.at(1,1) == 4 );
+  }
+}
+
+TEST_CASE( "Test changing elements out of range", "[matrix][access]" ) {
   SECTION("Try changing a value in an empty matrix"){
     matrix<int> m = {};
     REQUIRE_THROWS_AS( m.at(0,0) = 1, std::out_of_range );
