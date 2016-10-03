b8aae9db764f53f55f94dbaeb1cbc605d037307e
actinium <andreas.cederholm@gmail.com>
2016-10-04 00:07:31 +0200

    Add more resizing test cases.

--- test/test_matrix_modifiers.cpp
+++ test/test_matrix_modifiers.cpp
@@ -2,7 +2,7 @@
 #include "matrix.h"
 
 TEST_CASE( "Test matrix resize", "[matrix][modifiers]" ) {
-  SECTION("Compare empty matrixes"){
+  SECTION("Add column to matrix"){
     matrix<int> m1 = {{1,2},{3,4}};
     matrix<int> m2 = {{1,2,0},{3,4,0}};
     REQUIRE( m1.rows() == 2);
@@ -10,6 +10,56 @@ TEST_CASE( "Test matrix resize", "[matrix][modifiers]" ) {
     m1.resize(2,3);
     REQUIRE( m1.rows() == 2);
     REQUIRE( m1.columns() == 3);
+    REQUIRE( m1 == m2 );
+  }
+  SECTION("Add row to matrix"){
+    matrix<int> m1 = {{1,2},{3,4}};
+    matrix<int> m2 = {{1,2},{3,4},{0,0}};
+    REQUIRE( m1.rows() == 2);
+    REQUIRE( m1.columns() == 2);
+    m1.resize(3,2);
+    REQUIRE( m1.rows() == 3);
+    REQUIRE( m1.columns() == 2);
+    REQUIRE( m1 == m2 );
+  }
+  SECTION("Add row and column to matrix"){
+    matrix<int> m1 = {{1,2},{3,4}};
+    matrix<int> m2 = {{1,2,0},{3,4,0},{0,0,0}};
+    REQUIRE( m1.rows() == 2);
+    REQUIRE( m1.columns() == 2);
+    m1.resize(3,3);
+    REQUIRE( m1.rows() == 3);
+    REQUIRE( m1.columns() == 3);
+    REQUIRE( m1 == m2 );
+  }
+  SECTION("Remove column from matrix"){
+    matrix<int> m1 = {{1,2},{3,4}};
+    matrix<int> m2 = {{1},{3}};
+    REQUIRE( m1.rows() == 2);
+    REQUIRE( m1.columns() == 2);
+    m1.resize(2,1);
+    REQUIRE( m1.rows() == 2);
+    REQUIRE( m1.columns() == 1);
+    REQUIRE( m1 == m2 );
+  }
+  SECTION("Remove row from matrix"){
+    matrix<int> m1 = {{1,2},{3,4}};
+    matrix<int> m2 = {{1,2}};
+    REQUIRE( m1.rows() == 2);
+    REQUIRE( m1.columns() == 2);
+    m1.resize(1,2);
+    REQUIRE( m1.rows() == 1);
+    REQUIRE( m1.columns() == 2);
+    REQUIRE( m1 == m2 );
+  }
+  SECTION("Add row and column to matrix"){
+    matrix<int> m1 = {{1,2},{3,4}};
+    matrix<int> m2 = {{1}};
+    REQUIRE( m1.rows() == 2);
+    REQUIRE( m1.columns() == 2);
+    m1.resize(1,1);
+    REQUIRE( m1.rows() == 1);
+    REQUIRE( m1.columns() == 1);
     REQUIRE( m1 == m2 );
   }
 }
