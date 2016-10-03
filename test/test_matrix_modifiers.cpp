7f4b38b502c25bf3ff9bbbf6f13a280b671406dc
actinium <andreas.cederholm@gmail.com>
2016-10-04 00:01:21 +0200

    Add test for resize.

--- test/test_matrix_modifiers.cpp
+++ test/test_matrix_modifiers.cpp
@@ -3,8 +3,13 @@
 
 TEST_CASE( "Test matrix resize", "[matrix][modifiers]" ) {
   SECTION("Compare empty matrixes"){
-    matrix<int> m = {{1,2},{3,4}};
-    REQUIRE( m.rows() == 2);
-    REQUIRE( m.columns() == 2);
+    matrix<int> m1 = {{1,2},{3,4}};
+    matrix<int> m2 = {{1,2,0},{3,4,0}};
+    REQUIRE( m1.rows() == 2);
+    REQUIRE( m1.columns() == 2);
+    m1.resize(2,3);
+    REQUIRE( m1.rows() == 2);
+    REQUIRE( m1.columns() == 3);
+    REQUIRE( m1 == m2 );
   }
 }
