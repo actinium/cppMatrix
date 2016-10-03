feb8e72bc896134cd60a23e3403be983e473b31d
actinium <andreas.cederholm@gmail.com>
2016-10-04 00:00:59 +0200

    Add resize implementation.

--- include/matrix_modifiers_impl.h
+++ include/matrix_modifiers_impl.h
@@ -4,5 +4,17 @@
 
 template<class T>
 void matrix<T>::resize( std::size_t rows, std::size_t cols ){
-  // TODO
+  std::size_t old_rows = this->rows();
+  std::size_t old_cols = this->columns();
+  std::vector<T> new_data( rows * cols );
+  for(std::size_t r=0; r < old_rows; ++r){
+    for(std::size_t c=0; c < old_cols; ++c){
+      if( r < rows && c < cols ){
+        new_data[r*cols+c] = data_[r*old_cols+c];
+      }
+    }
+  }
+  rows_ = rows;
+  cols_ = cols;
+  data_ = new_data;
 }
