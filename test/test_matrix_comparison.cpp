5b1a4aba18d0d849ff4c8684f9d2d16d3683ae29
actinium <andreas.cederholm@gmail.com>
2016-10-04 02:24:55 +0200

    Fix spelling.

--- test/test_matrix_comparison.cpp
+++ test/test_matrix_comparison.cpp
@@ -2,22 +2,22 @@
 #include "matrix.h"
 
 TEST_CASE( "Test matrix comparison", "[matrix][comparison]" ) {
-  SECTION("Compare empty matrixes"){
+  SECTION("Compare empty matrices"){
     matrix<int> m1 = {};
     matrix<int> m2 = {};
     REQUIRE( m1 == m2 );
   }
-  SECTION("Compare 1x1 matrixes"){
+  SECTION("Compare 1x1 matrices"){
     matrix<int> m1 = {{0}};
     matrix<int> m2(1,1);
     REQUIRE( m1 == m2 );
   }
-  SECTION("Compare 2x2 matrixes"){
+  SECTION("Compare 2x2 matrices"){
     matrix<int> m1 = {{1,2},{3,4}};
     matrix<int> m2 = {{1,2},{3,4}};
     REQUIRE( m1 == m2 );
   }
-  SECTION("Compare different 2x2 matrixes"){
+  SECTION("Compare different 2x2 matrices"){
     matrix<int> m1 = {{1,2},{3,4}};
     matrix<int> m2 = {{1,1},{2,2}};
     REQUIRE( m1 != m2 );
