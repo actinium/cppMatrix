34eda618d032ef8f1f53f73d98cdc45b22f62e3d
actinium <andreas.cederholm@gmail.com>
2016-10-09 02:21:44 +0200

    Add resizing testcase.

--- test/test_matrix_modifiers.cpp
+++ test/test_matrix_modifiers.cpp
@@ -52,7 +52,7 @@ TEST_CASE( "Test matrix resize", "[matrix][modifiers]" ) {
     REQUIRE( m1.columns() == 2);
     REQUIRE( m1 == m2 );
   }
-  SECTION("Add row and column to matrix"){
+  SECTION("Remove row and column to matrix"){
     matrix<int> m1 = {{1,2},{3,4}};
     matrix<int> m2 = {{1}};
     REQUIRE( m1.rows() == 2);
@@ -60,6 +60,16 @@ TEST_CASE( "Test matrix resize", "[matrix][modifiers]" ) {
     m1.resize(1,1);
     REQUIRE( m1.rows() == 1);
     REQUIRE( m1.columns() == 1);
+    REQUIRE( m1 == m2 );
+  }
+  SECTION("Add row and remove column to matrix"){
+    matrix<int> m1 = {{1,2,3},{4,5,6},{7,8,9}};
+    matrix<int> m2 = {{1,2},{4,5},{7,8},{0,0}};
+    REQUIRE( m1.rows() == 3);
+    REQUIRE( m1.columns() == 3);
+    m1.resize(4,2);
+    REQUIRE( m1.rows() == 4);
+    REQUIRE( m1.columns() == 2);
     REQUIRE( m1 == m2 );
   }
 }
