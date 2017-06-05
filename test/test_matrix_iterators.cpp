ec4b240e25502687bc046a6b2851a20d8ca0a530
Andreas Cederholm <andreas.cederholm@gmail.com>
2017-06-05 16:35:52 +0200

    Add more tests of iterators.

--- test/test_matrix_iterators.cpp
+++ test/test_matrix_iterators.cpp
@@ -28,6 +28,32 @@ TEST_CASE( "Test matrix begin and end", "[matrix][iterators]" ) {
   }
 }
 
+TEST_CASE( "Test changing element via iterator", "[matrix][iterators]" ) {
+  SECTION("Change elements in 3x3 matrix"){
+    mat::matrix<int> m1 = {{11,12,13},
+                           {21,22,23},
+                           {31,32,33}};
+    mat::matrix<int> m2 = {{ 1,12,13},
+                           {21, 2,23},
+                           {31,32, 3}};
+    auto b = m1.begin();
+    auto e = m1.end();
+    *b = 1;
+    b[4] = 2;
+    *(e-1) = 3;
+    REQUIRE( m1 == m2);
+  }
+  SECTION("Change elements in 1x3 matrix"){
+    mat::matrix<bool> m1 = {{true,false,false}};
+    mat::matrix<bool> m2 = {{false,false,true}};
+    auto b = m1.begin();
+    auto e = m1.end();
+    *b = !(*b);
+    *(e-1) = !*(e-1);
+    REQUIRE( m1 == m2 );
+  }
+}
+
 TEST_CASE( "Test matrix rbegin and rend", "[matrix][iterators]" ) {
   SECTION("Iterate over elements in 3x3 matrix"){
     mat::matrix<int> m = {{11,12,13},
